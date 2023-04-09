5ca1b5241a41b10002078d128eb7cb3485abc881
Iva Havranová <havranova.i@seznam.cz>
2023-04-09 13:15:20 +0200

    Úkol hotov

--- index.js
+++ index.js
@@ -23,7 +23,8 @@ console.log('funguju');
 // Vytvořte si repozitář ze šablony cviceni-email a pojmenujte ho email2-zadani.
 // Do souboru index.js vložte kód vzorového řešení. Na tomto kódu budeme dále stavět.
 const goodbye = (name) => {
-  return 'S pozdravem ' + name;
+  const closingElm = document.querySelector('.email__closing');
+  return (closingElm.textContent = 'S pozdravem ' + name);
 };
 
 const fillSubject = (subject) => {
@@ -32,34 +33,38 @@ const fillSubject = (subject) => {
 
 // Všimněte si funkce goodbye, která generuje pozdrav na konec e-mailu. Přidejte alespoň dvě další funkce, kde každá generuje k zadanému jménu jiný typ pozdravu. Například formalGoodbye pro velmi formální pozdravy jako „S uctivou poklonou…“, nebo naopak rudeGoodbye typu „Se měj…“, pokud se chcete rozloučit nevybíravě.
 const formalGoodbye = (name) => {
-  return 'S úctou ' + name;
+  const closingElm = document.querySelector('.email__closing');
+  return (closingElm.textContent = 'S úctou ' + name);
 };
 const rudeGoodbye = (name) => {
-  return 'Nazdar, ' + name;
+  const closingElm = document.querySelector('.email__closing');
+  return (closingElm.textContent = 'Nazdar, ' + name);
 };
 // Udělejte z funkce fillBody funkci vyššího řádu. Přidejte jí třetí parametr goodbyeFunction, který představuje funkci, pomocí které se má vygenerovat závěrečný pozdrav. Vyzkoušejte zavolat funkci fillBody postupně s každou z vašich zdravících funkcí a ověřte, že vše správně funguje.
-// Ukázka použití
-// fillSubject('Obchodní sdělení');
-// fillBody(
-//   'Kupte mycí prostředek, který vám vytře zrak, se slevou 50 %.',
-//   'Jan Čistý',
-//   formalGoodbye
-// );
-// nebo
-
-// fillSubject('Pozvánka na oslavu narozenin');
-// fillBody('Zítra oslava. 18:00 ve Starý hospodě.', 'Patrik Veselý', rudeGoodbye);
 const fillBody = (body, name, goodbyeFunction) => {
   const bodyElm = document.querySelector('.email__body');
   bodyElm.innerHTML = body;
-  const closingElm = document.querySelector('.email__closing');
   closingElm.textContent = goodbye(name);
 };
+// Ukázka použití
+// fillSubject('Spam');
+// fillBody(
+//   'Kupte mycí prostředek, který vám vytře zrak, se slevou 50 %.',
+//   rudeGoodbye('Jan Čistý'),
+// );
+// nebo
 
-console.log(
-  fillBody(
-    'Zítra oslava. 18:00 ve Starý hospodě.',
-    'Patrik Veselý',
-    rudeGoodbye,
-  ),
+fillSubject('Pozvánka na oslavu narozenin');
+fillBody(
+  'Zítra oslava. 18:00 ve Starý hospodě.',
+  'Patrik Veselý',
+  rudeGoodbye('Patrik Veselý'),
 );
+
+// console.log(
+//   fillBody(
+//     'Zítra oslava. 18:00 ve Starý hospodě.',
+//     'Patrik Veselý',
+//     rudeGoodbye,
+//   ),
+// );
